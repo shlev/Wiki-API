e748354fe2c79db335974cb1bc6923bdc0a02c75
shlomi <shl.lev.ks@gmail.com>
2021-07-09 12:18:36 +0300

    add articles route (get,post,delete)

--- app.js
+++ app.js
@@ -25,6 +25,39 @@ const articlesSchema = {
 
 const Article = mongoose.model("Article", articlesSchema);
 
+app.route('/articles')
+    .get( function(req, res) {
+        Article.find( function(err, articles){
+            if (!err) {
+                res.send(articles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post( function(req,res) {
+        const titleInput = req.body.title;
+        const contentInput = req.body.content;
+        console.log(titleInput, contentInput);
+        const newArticle = new Article({title: titleInput, content: contentInput});
+        newArticle.save(function(err) {
+            if ( !err ) {
+                res.send('Successfully added a new article');
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function(req,res) {
+        Article.deleteMany(function(err) {
+            if (!err) {
+                res.send('Successfully deleted all articles');
+            } else {
+                res.send(err);
+            }
+        })
+    });
+
 app.listen(PORT, function() {
     console.log(`Server started on port ${PORT}`);
 })
